faef71876c8b3d2259dfe103c922f04bfbe6ae14
pituganov <pituganovstas@gmail.com>
2019-08-25 19:58:05 +0500

    Add dotenv to requirements

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     keywords='progressbar progressmeter progress bar meter'
              ' rate eta console terminal time telegram',
     packages=['telelog'] + ['telelog.' + i for i in find_packages('telelog')],
-    install_requires=['tqdm', 'telepot'],
+    install_requires=['tqdm', 'telepot', 'dotenv'],
     project_urls={
         'Source': 'https://github.com/ermakovpetr/tg_tqdm/',
     },
